e4b2dbb6318232babb4b8dbb0f43954374c02141
Richard Kneale <richardkneale@hotmail.co.uk>
2019-06-29 16:19:03 +0100

    Added camera follow script

---
+++ BeginnerScripting/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speedMove;
+    public float powerJump;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(speedMove * moveHorizontal, powerJump * jump, speedMove * moveVertical);
+
+        rb.AddForce(movement);
+    }
+}

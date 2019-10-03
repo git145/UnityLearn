31ad04fd0098f4aa82a37973657aca1c2075e945
Richard Kneale <richardkneale@hotmail.co.uk>
2019-10-03 22:26:10 +0100

    Updated text input

---
+++ Text_Adventure/Assets/Scripts/TextInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour
+{
+    public InputField _inputField;
+
+    private GameController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<GameController>();
+
+        _inputField.onEndEdit.AddListener(AcceptStringInput);
+    }
+
+    private void AcceptStringInput(string userInput)
+    {
+        userInput = userInput.ToLower();
+
+        _controller.LogStringWithReturn(userInput);
+
+        InputComplete();
+    }
+
+    private void InputComplete()
+    {
+        _controller.DisplayLoggedText();
+
+        _inputField.ActivateInputField();
+
+        _inputField.text = null;
+    }
+}
